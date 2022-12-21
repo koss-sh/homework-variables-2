e7c4cecf5cce9e6ca9d297e4a0f452600371a2d4
koss-sh <shilinskii22@gmail.com>
2022-12-21 20:44:00 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int a = 500;
+        byte b = 10;
+        short c = 10000;
+        long d = 100000L;
+        float e = 1.5f;
+        double f = 12.258;
+        System.out.println("Значение переменной a с типом int равно " + a);
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        System.out.println("Значение переменной c с типом short равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
+    }
+}
